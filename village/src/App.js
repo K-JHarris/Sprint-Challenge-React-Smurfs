f36a8685995bfe02beb2050ba069892e9c5aa2d6
Kay Jay <kharris2k16@gmail.com>
2019-06-15 09:26:51 -0700

    Get request successful, retrieving data from server

--- village/src/App.js
+++ village/src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
 
+import axios from 'axios'
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -11,10 +13,18 @@ class App extends Component {
       smurfs: [],
     };
   }
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  // Notice what your map function is looping over and returning inside of Smurfs.
-  // You'll need to make sure you have the right properties on state and pass them down to props.
+
+  componentDidMount(){
+    axios 
+      .get('http://localhost:3333/smurfs')
+      .then(res => this.setState({
+        smurfs: res.data
+      }))
+      .catch(err => console.log("no this is not the way" + err))
+  }
+
   render() {
+    console.log(this.state.smurfs)
     return (
       <div className="App">
         <SmurfForm />
@@ -26,4 +36,7 @@ class App extends Component {
 
 export default App;
 
-// good morning
+// good morning 
+// add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
+// Notice what your map function is looping over and returning inside of Smurfs.
+// You'll need to make sure you have the right properties on state and pass them down to props.
